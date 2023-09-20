ffd9e78b577d4d7670b37a400b7af509fe85d542
sugarshock <matthias.bauer72@gmail.com>
2023-09-20 14:36:04 +0200

    finished

--- algorithmservice/src/main/java/de/wirschiffendas/algorithmservice/kafka/AnalyzerJobListener.java
+++ algorithmservice/src/main/java/de/wirschiffendas/algorithmservice/kafka/AnalyzerJobListener.java
@@ -3,6 +3,7 @@ package de.wirschiffendas.algorithmservice.kafka;
 import de.wirschiffendas.algorithmservice.controller.AlgorithmController;
 import de.wirschiffendas.shared_classes.data.entity.AnalyzerJob;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +12,8 @@ public class AnalyzerJobListener {
 
     @Autowired
     AlgorithmController algorithmController;
-    @KafkaListener(topics = "analyzer", groupId = "1", containerFactory = "jobUpdateListenerContainerFactory")
+
+    @KafkaListener(topics = "analyzer", containerFactory = "jobUpdateListenerContainerFactory")
     public void listenEquipJobUpdate(AnalyzerJob message) {
         algorithmController.analyze(message);
     }
